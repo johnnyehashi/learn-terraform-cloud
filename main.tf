0ec289c89e2d6bb9daa4ad531d3b2eb3317063a5
johnnyehashi <156373071+johnnyehashi@users.noreply.github.com>
2024-12-05 07:06:53 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,22 +22,9 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = var.instance_name
-  }
-}
-
-resource "aws_instance" "ubuntu2" {
+  count = 500
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 }
 
-resource "aws_instance" "ubuntu3" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-}
 
-resource "null_resource" "tfc-pls-apply" { }
